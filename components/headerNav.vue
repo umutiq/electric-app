4050b7b6fc205daee0e43ffdcd2223a12afee8c6
Umut Aktaş <umut@iqmotion.com>
2022-11-17 17:06:16 +0300

    ?

--- components/headerNav.vue
+++ components/headerNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex justify-between items-center px-12 bg-gray-100 py-2">
-    <div class="text-3xl">Arinity Solar</div>
+    <div class="text-3xl">Arinniti Solar</div>
   </div>
 </template>
 
